a86b3da3e735a895480ca42179dc50ff9d261acb
cosmear <92765555+cosmear@users.noreply.github.com>
2026-03-03 17:26:52 -0300

    cambio de iconos

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="es" className={`${inter.variable}`}>
             <head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=villa,dashboard,error,login,progress_activity,location_on,bed,shower,square_foot,add_home,delete_sweep,arrow_forward,arrow_outward,search_off,slow_motion_video,logout,send" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
             </head>
             <body className="bg-cream text-stone-dark font-display min-h-screen flex flex-col antialiased">
